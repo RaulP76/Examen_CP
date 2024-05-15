024e4e50053f307de3000572c33de91ce1e49704
RaulP76 <115876885+RaulP76@users.noreply.github.com>
2024-05-15 16:24:07 +0300

    Update Problema C++.cpp

--- Problema C++.cpp
+++ Problema C++.cpp
@@ -1,33 +1,33 @@
-#include <iostream> //biblioteca necesara pentru datele de intrare/iesire in C++
-#include <ctime>    //biblioteca necesara pentru a utiliza funtia " time "
-#include <cstdlib>  //biblioteca necesara pentru a utiliza funtia " rand "
-using namespace std;//declaratie pentru folosirea de nume din biblioteca standard
+#include <iostream> 
+#include <ctime>    
+#include <cstdlib>  
+using namespace std;
 
-int main(){//declararea functiei principale
+int main(){
 //generarea de numere aleatoare de la 0 la 10
     srand (time(NULL));//functiile 'strand', 'time' si 'NULL' folosite pentru generarea de numere aleatoare
     int n= (rand()%10+1), r;//declarare si initializare variabile ( aleatoare pentru n si r pentru numarul de publicatii ce trebuie returnate )
-    cout << "Numarul de publicatii imprumutate este: " << n << endl;//afisare pe consola
+    cout << "Numarul de publicatii imprumutate este: " << n << endl;
 
 //conditii necesare pentru rezolvarea problemei
-    if ( n % 2 == 0 ) {//implementare conditie daca numarul de publicatii imprumutate este divizibil cu 2
-        r = n/2;//functia care stabileste cate publicatii trebuie returnate
-        cout << "Utilizatorul este rugat sa restituie " << r << " publicatii.\n";//afisare pe consola
+    if ( n % 2 == 0 ) {
+        r = n/2;
+        cout << "Utilizatorul este rugat sa restituie " << r << " publicatii.\n";
 
-        } else if ( n % 3 == 0 ) {//implementare conditie daca numarul de publicatii imprumutate este divizibil cu 3
-            r = n*2/3;//functia care stabileste cate publicatii trebuie returnate
-            cout << "Utilizatorul este rugat sa restituie " << r << " publicatii.\n";  //afisare pe consola
+        } else if ( n % 3 == 0 ) {
+            r = n*2/3;
+            cout << "Utilizatorul este rugat sa restituie " << r << " publicatii.\n"; 
 
-            } else if ( n % 3 == 0 && n % 2 == 0 ) {//implementare conditie daca numarul de publicatii imprumutate este divizibil cu 2 si cu 3
-                r=n/2+1;//functia care stabileste cate publicatii trebuie returnate
-                cout << "Utilizatorul este rugat sa restituie " << r << " publicatii.\n";//afisare pe consola
+            } else if ( n % 3 == 0 && n % 2 == 0 ) {
+                r=n/2+1;
+                cout << "Utilizatorul este rugat sa restituie " << r << " publicatii.\n";
 
-                } else if ( n % 3 != 0 || n % 2 != 0 ) {//implementare conditie daca numarul de publicatii imprumutate este divizibil cu 2 sau cu 3
-                    r=n;//functia care stabileste cate publicatii trebuie returnate
-                    cout << "Utilizatorul este rugat sa restituie " << r << " publicatii.\n";//afisare pe consola
-                    } else  {//executare conditie pe ramura de fals daca celelalte condtii nu sunt adevarate
-                        cout << "La revedere!\n";//afisare pe consola
+                } else if ( n % 3 != 0 || n % 2 != 0 ) {
+                    r=n;
+                    cout << "Utilizatorul este rugat sa restituie " << r << " publicatii.\n";
+                    } else  {
+                        cout << "La revedere!\n";
                         }
 
-    return 0;//incheierea functiei principale
+    return 0;
 }
